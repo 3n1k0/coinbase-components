e395a93675a0d605ad5389eb2ca0d6a2e5469659
Eniko Kosztolanyi <32710850+3n1k0@users.noreply.github.com>
2021-06-29 10:11:53 +0200

    add css modules and connect currency data

---
+++ src/currencyData.js
@@ -0,0 +1,21 @@
+export const currencies = [
+  {
+    name: "Bitcoin",
+    subtitle: "BTC",
+    image:
+      "https://dynamic-assets.coinbase.com/e785e0181f1a23a30d9476038d9be91e9f6c63959b538eabbc51a1abc8898940383291eede695c3b8dfaa1829a9b57f5a2d0a16b0523580346c6b8fab67af14b/asset_icons/b57ac673f06a4b0338a596817eb0a50ce16e2059f327dc117744449a47915cb2.png",
+    price: "€29,375.88",
+    change: "+1.22%",
+    marketCap: "€550.9B",
+  },
+
+  {
+    name: "Somecoin",
+    subtitle: "BTC",
+    image:
+      "https://dynamic-assets.coinbase.com/e785e0181f1a23a30d9476038d9be91e9f6c63959b538eabbc51a1abc8898940383291eede695c3b8dfaa1829a9b57f5a2d0a16b0523580346c6b8fab67af14b/asset_icons/b57ac673f06a4b0338a596817eb0a50ce16e2059f327dc117744449a47915cb2.png",
+    price: "€29,375.88",
+    change: "+1.22%",
+    marketCap: "€550.9B",
+  },
+];
--- src/tradePage.js
+++ src/tradePage.js
@@ -1,50 +1,97 @@
-import "./tradePage.css";
+import styles from "./tradePage.module.css";
+import { currencies } from "./currencyData";
 
 function tradePage() {
   return (
     <>
-      <div className="trade"></div>
-      <div className="side-bar"></div>
-      <div className="desktop-header"></div>
-      <div className="main-container">
-        <div className="table-container">
-          <div className="search-bar"></div>
-          <table className="asset-table">
+      <div className={styles.trade}></div>
+      <div className={styles.sideBar}></div>
+      <div className={styles.desktopHeader}></div>
+      <div className={styles.mainContainer}>
+        <div className={styles.tableContainer}>
+          <table className={styles.assetTable}>
             <thead>
+              <tr className={styles.searchBar}></tr>
               <tr>
-                <th class="thead-name">Name</th>
-                <th>Price</th>
-                <th>Change</th>
-                <th class="thead-marketcap">Market cap</th>
-                <th></th>
-                <th>Watch</th>
+                <th className={styles.theadName}>Name</th>
+                <th className={styles.theadPrice}>Price</th>
+                <th className={styles.theadChange}>Change</th>
+                <th className={styles.theadMarketcap}>
+                  Market cap
+                  <svg
+                    width="6"
+                    height="8"
+                    viewBox="0 0 6 8"
+                    fill="none"
+                    reversed=""
+                    className={styles.sortIcon}
+                  >
+                    <path
+                      d="M3.167 0l2.5 2.828h-5L3.167 0z"
+                      fill="#B8C2CC"
+                    ></path>
+                    <path
+                      d="M3.167 8l-2.5-2.828h5L3.167 8z"
+                      fill="#1652F0"
+                    ></path>
+                  </svg>
+                </th>
+                <th className={styles.theadButtonContainer}></th>
+                <th className={styles.theadWatch}>Watch</th>
               </tr>
             </thead>
             <tbody>
-              <tr class="asset-item">
-                <th class="asset-image"></th>
-                <th class="asset-subtitle"></th>
-                <th class="asset-price"></th>
-                <th class="asset-change"></th>
-                <th class="asset-marketcap"></th>
-                <th class="asset-star"></th>
-              </tr>
-              <tr class="asset-item">
-                <th class="asset-image"></th>
-                <th class="asset-subtitle"></th>
-                <th class="asset-price"></th>
-                <th class="asset-change"></th>
-                <th class="asset-marketcap"></th>
-                <th class="asset-star"></th>
-              </tr>
-              <tr class="asset asset-item">
-                <th class="asset-image"></th>
-                <th class="asset-subtitle"></th>
-                <th class="asset-price"></th>
-                <th class="asset-change"></th>
-                <th class="asset-marketcap"></th>
-                <th class="asset-star"></th>
-              </tr>
+              {currencies.map((currency) => {
+                return (
+                  <tr
+                    className={styles.asset + " " + styles.assetItem}
+                    key={currency.name}
+                  >
+                    <th className={styles.assetName}>
+                      <div className={styles.assetName}>{currency.name}</div>
+                      <div className={styles.assetSubtitle}></div>
+                      <div className={styles.assetImage}></div>
+                    </th>
+                    <th className={styles.assetPrice}></th>
+                    <th className={styles.assetChange}></th>
+                    <th className={styles.assetMarketcap}></th>
+                    <th className={styles.assetBuy}>
+                      <button className={styles.buyButton}>Buy</button>
+                    </th>
+                    <th className={styles.assetStar}>
+                      <svg
+                        fill="none"
+                        height="23"
+                        viewBox="0 0 24 23"
+                        width="24"
+                        filled="1"
+                        color="#1652F0"
+                        className={styles.starIcon}
+                      >
+                        <svg
+                          fill="none"
+                          height="23"
+                          viewBox="0 0 24 23"
+                          width="24"
+                          filled="1"
+                          color="#1652F0"
+                          className={styles.starIcon}
+                        >
+                          <path
+                            d="M12.713 1.443l2.969 6.015 6.637.965a.794.794 0 01.44 1.354l-4.804 4.681 1.135 6.612a.794.794 0 01-1.152.837L12 18.787l-5.938 3.121a.795.795 0 01-1.152-.838l1.134-6.612L1.24 9.777a.794.794 0 01.44-1.354l6.638-.965 2.968-6.015a.795.795 0 011.425 0z"
+                            stroke="#becada"
+                          ></path>
+                        </svg>
+                        <path
+                          d="M12.713 1.443l2.969 6.015 6.637.965a.794.794 0 01.44 1.354l-4.804 4.681 1.135 6.612a.794.794 0 01-1.152.837L12 18.787l-5.938 3.121a.795.795 0 01-1.152-.838l1.134-6.612L1.24 9.777a.794.794 0 01.44-1.354l6.638-.965 2.968-6.015a.795.795 0 011.425 0z"
+                          stroke="#becada"
+                        ></path>
+                      </svg>
+                    </th>
+                  </tr>
+                );
+              })}
+              
             </tbody>
           </table>
         </div>
